068257702926f6052a8300b3f25110b9720c9ddc
Elena Kolesnik <kolesnick.e@gmail.com>
2025-12-18 23:11:52 +0300

    Fix Flexmonster: remove watermarks and set fullscreen height

--- dashboard/app/urls.py
+++ dashboard/app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.home_page, name='homepage'),
     path('report/', views.report_page, name='report'),
     path('login/', views.login_page, name='login'),
-    # Добавляем маршруты для регистрации и выхода
     path('register/', views.register_page, name='register'),
     path('logout/', views.logout_view, name='logout'),
 ]
